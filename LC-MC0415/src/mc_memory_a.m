439d5f527f7c8fb888d9024152b9858cc7ffda7d
niam-foxel <nils.hamel@foxel.ch>
2015-07-06 10:44:15 +0200

    Plot font size correction and model estimator removal (bad measures)

--- LC-MC0415/src/mc_memory_a.m
+++ LC-MC0415/src/mc_memory_a.m
@@ -73,6 +73,6 @@
         set( gcf, 'PaperPosition', [0 0 20 10] );
 
         % Export plot in color EPS format %
-        print( '-depsc', '-F:12', [ '../dev/plots/mc_memory_a.eps' ] );
+        print( '-depsc', '-F:16', [ '../dev/plots/mc_memory_a.eps' ] );
 
     end
--- LC-MC0415/src/mc_storage_a.m
+++ LC-MC0415/src/mc_storage_a.m
@@ -73,6 +73,6 @@
         set( gcf, 'PaperPosition', [0 0 20 10] );
 
         % Export plot in color EPS format %
-        print( '-depsc', '-F:12', [ '../dev/plots/mc_storage_a.eps' ] );
+        print( '-depsc', '-F:16', [ '../dev/plots/mc_storage_a.eps' ] );
 
     end
